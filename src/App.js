190117e183bca71da823d316235e710816a8514a
aman527 <amanchoudhri@gmail.com>
2020-05-28 17:11:52 -0400

    Create landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Landing from './Landing.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Landing />
       <h1>Globeslayers</h1>
-      <h5>The Truth: Plane and Simple</h5>
+      <h3>The Truth: Plane and Simple</h3>
     </div>
   );
 }
---
+++ src/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Landing() {
+    return (
+        <div class = "landing">
+            <h1>The world as we know it is under threat.</h1>
+        </div>
+    )
+}
+
+export default Landing
