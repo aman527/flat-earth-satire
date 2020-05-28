b89c3d1d8dcd5c0f02ac6648176c02dca105cc6b
aman527 <amanchoudhri@gmail.com>
2020-05-28 15:41:54 -0400

    Initial commit

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+function Nav () {
+    return (
+        <nav>
+            <p className="title">The Knight of Truth</p>
+            <ul className = "nav-ul">
+                <a href="/" className="nav-link">Home</a>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
