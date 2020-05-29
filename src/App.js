215b02a79097bde128b8f744e7c29c0826c2a7f6
aman527 <amanchoudhri@gmail.com>
2020-05-28 20:07:59 -0400

    Start work on Landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Landing from './Landing.js'
+import Blog from './Blog.js'
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Landing />
-      <h1>Globeslayers</h1>
-      <h3>The Truth: Plane and Simple</h3>
+      <Router>
+        <Route path = "/" exact component = {Landing}></Route>
+        <Route path = "/blog" component = {Blog}></Route>
+      </Router>
     </div>
   );
 }
---
+++ src/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Blog() {
+    return (
+        <div class = "blog">
+            <h1>Exposing the Con-sphere-acy</h1>
+        </div>
+    )
+}
+
+export default Blog
