918886d64d15ad7e7ed39175282e847d42317b7b
aman527 <amanchoudhri@gmail.com>
2020-05-31 19:40:39 -0400

    Emphasize text in about page

--- src/About.js
+++ src/About.js
@@ -17,7 +17,7 @@ function About () {
                     <h2>Who We Are</h2>
                     <p><em style = {{color: "#087E8B", fontWeight: 'bold'}}>Conspiracy Slayers</em> is a collection of individuals determined to seek the truth at all costs. We're convinced that the United States government is operating a "global" conspiracy to swindle the public into believing the Earth is round.</p>
                     <p>We aren't fooled by their convoluted lies and elaborate deceptions. Lives dominated by paranoia have trained us to believe nothing and trust even less, except for that which passes through our careful scrutiny.</p>
-                    <p>We're very thorough, making sure to consider all favorable evidence before formulating a position, but we really don't need to. We know we're correct. How, you might ask? Simple. Anything disagrees with our view is just a government ploy.</p>
+                    <p>We're very thorough, making sure to consider all favorable evidence before formulating a position, but we really don't need to. We know we're correct. How, you might ask? Simple. Anything that disagrees with our view is <span className = "emphasized">just a government ploy.</span></p>
                     
                 </div>
             </div>
